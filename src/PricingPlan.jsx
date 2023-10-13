75047178762f3e3ca85347d7d0e5f6040e09eb3a
ArjunBhardwaj3 <130498803+ArjunBhardwaj3@users.noreply.github.com>
2023-10-14 03:11:58 +0530

    PricingPlan.jsx

--- src/PricingPlan.jsx
+++ src/PricingPlan.jsx
@@ -4,24 +4,29 @@ import './PricingPlan.css'
 
 function PricingPlan() {
   return (
+    
     <div>
       <h1>Pricing Plans</h1>
+     
       <div className="plan">
         <h2>Free Plan</h2>
-        <p>Basic features</p>
-        <p>Price: $0/month</p>
-        <p>Currently Using</p>
+        <p>Basic features
+           
+        Price: Rs 0/month</p>
+        <Link to="">Free</Link>
       </div>
       <div className="premplan">
         <h2>Premium Plan</h2>
-        <p>Advanced features</p>
-        <p>Price: 999/month</p>
+        <p>Advanced features
+            <br/>
+        Price: Rs 999/month</p>
         <Link to="https://buy.stripe.com/test_5kA6qI7HvaCQ6NGdQQ">Select Premium Plan</Link>
       </div>
       <div className='goldplan'>
         <h2>Gold Plan</h2>
-        <p>All Features + Extra benefits</p>
-        <p>Price: 9999/year</p>
+        <p>All Features + Extra benefits      
+            <br/>
+            Price: Rs 9999/year</p>
         <Link to="https://buy.stripe.com/test_9AQ16oaTHbGU1tm7st">Select Gold Plan</Link>
       </div>
     </div>
